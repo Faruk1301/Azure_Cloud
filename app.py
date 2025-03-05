d65b26d9711ba70751d226eff320a6767a5bae45
Md.Faruk Hossain <faruk.cse.pust12@gmail.com>
2025-03-05 12:01:00 +0600

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Azure & GitHub Actions! i edit this file 5 times "
+    return "Hello from Azure & GitHub Actions! i edit this file 6 times "
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
