720e2614836b50c39dc05c3a912ccf8394075ea7
Md.Faruk Hossain <faruk.cse.pust12@gmail.com>
2025-03-06 00:41:59 +0600

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Azure! This is a simple Python script running with Flask. From Bangladesh, England, and ITALY."
+    return "Hello, Azure! This is a simple Python script running with Flask. From Bangladesh, England, and CANA."
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
