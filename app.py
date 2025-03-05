52d729a6757f2139e8e43e27ea7cb03f95bb6a2a
Md.Faruk Hossain <faruk.cse.pust12@gmail.com>
2025-03-05 23:52:44 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # app.py
 
 def main():
-    print("Hello, Azure! This is a simple Python script running without Flask. from bangladesh ")
+    print("Hello, Azure! This is a simple Python script running without Flask. from bangladesh and england ")
 
 if __name__ == '__main__':
     main()
