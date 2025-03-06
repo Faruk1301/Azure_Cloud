abc0993dd58cf12267e5416828b8794cb55da1cb
Md.Faruk Hossain <faruk.cse.pust12@gmail.com>
2025-03-06 09:24:23 +0600

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Azure! This is a simple Python script running   "
+    return "Hello, Azure! This is a simple Python script running from  scratch "
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)  # This will run the built-in server
