4134d56b072f20f5bd0893c12a497e180d3bf102
Md.Faruk Hossain <faruk.cse.pust12@gmail.com>
2025-03-12 00:42:08 +0600

    Update demo.tf

--- demo.tf
+++ demo.tf
@@ -1,7 +1,7 @@
 Terraform will perform the following actions:
   + resource "azurerm_resource_group" "rg" {
       + location = "East US"
-      + name     = "RG1"
+      + name     = "RG2"
     }
 Plan: 1 to add, 0 to change, 0 to destroy.
 
