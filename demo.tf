3058f32d03fac973bde6e285f48ba1a8d66e9ce9
Md.Faruk Hossain <faruk.cse.pust12@gmail.com>
2025-03-11 18:12:19 +0000

    Add demo.tf file with initial Terraform code

---
+++ demo.tf
@@ -0,0 +1,6 @@
+Terraform will perform the following actions:
+  + resource "azurerm_resource_group" "rg" {
+      + location = "East US"
+      + name     = "RG1"
+    }
+Plan: 1 to add, 0 to change, 0 to destroy.
