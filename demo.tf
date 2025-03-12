674b3f723f7e45488ef22f0e2e0aa18d3bcf4397
Md.Faruk Hossain <faruk.cse.pust12@gmail.com>
2025-03-12 11:14:38 +0600

    Update demo.tf

--- demo.tf
+++ demo.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
 resource "azurerm_resource_group" "rg" {
   name     = "RG3"
   location = "East US"
@@ -7,6 +20,7 @@ resource "azurerm_resource_group" "rg" {
     department  = "IT"
   }
 }
+
 
 
 
