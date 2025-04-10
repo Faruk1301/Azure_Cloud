ae1d5a4b3d3aa756950347b8c524cffdf8c308b7
Md.Faruk Hossain <faruk.cse.pust12@gmail.com>
2025-04-10 15:46:01 +0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,8 +17,8 @@ resource "azurerm_resource_group" "example" {
 }
 
 resource "azurerm_virtual_network" "example" {
-  name                = "example-vnet"
-  address_space       = ["40.0.0.0/16"]
+  name                = "example-vnet1"
+  address_space       = ["50.0.0.0/16"]
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 }
